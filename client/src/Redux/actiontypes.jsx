39c38aabaff10027de2fc1b0c8dde9024e739a30
Frango113 <frangoat38@aol.com>
2023-10-26 15:28:36 -0300

    26/10

---
+++ client/src/Redux/actiontypes.jsx
@@ -0,0 +1,10 @@
+export const DOGBYNAME = "DOGBYNAME";
+export const DOGBYID = "DOGBYID";
+export const DOG_ERROR = "DOG_ERROR";
+export const ALLDOGS = "ALLDOGS";
+export const ORDER = "ORDER";
+export const FILTER_TEMP = "FILTER_TEMP";
+export const ALLTEMP = "ALLTEMP";
+export const CLEAR_DOGS = "CLEAR_DOGS";
+export const FILTER_ORIGIN = "FILTER_ORIGIN";
+export const FILTER_LIFE = "FILTER_LIFE";
---
+++ client/src/Redux/reducer.jsx
@@ -0,0 +1,284 @@
+import {
+  DOGBYNAME,
+  DOGBYID,
+  DOG_ERROR,
+  ALLDOGS,
+  ORDER,
+  FILTER_TEMP,
+  ALLTEMP,
+  CLEAR_DOGS,
+  FILTER_ORIGIN,
+  FILTER_LIFE,
+} from "../Redux/actiontypes";
+
+let initialState = {
+  dogs: [],
+  allDogs: [],
+  temperament: [],
+  searchError: false,
+  orderAndFilter: { order: "A", tempFilter: "All", originFilter: "all" },
+};
+
+export default function rootReducer(state = initialState, action) {
+  const { payload, type } = action;
+  switch (type) {
+    case DOGBYNAME:
+      return {
+        ...state,
+        dogs: payload,
+        allDOgs: payload,
+        searchError: false,
+        orderAndFilter: {
+          order: "A",
+          tempFilter: "All",
+          originFilter: "all",
+        },
+      };
+    case DOGBYID:
+      return {
+        ...state,
+        dogs: payload,
+        allDogs: payload,
+        searchError: false,
+      };
+    case ALLDOGS:
+      let result = [];
+      payload.forEach((dog) => {
+        let words = dog.temperament.split(", ");
+        let firstWord = words[0];
+        if (!result.includes(firstWord)) {
+          result.push(firstWord);
+        }
+      });
+      return {
+        ...state,
+        allDogs: payload,
+        dogs: payload,
+        temperament: result,
+      };
+    case CLEAR_DOGS:
+      return {
+        ...state,
+        allDogs: [],
+        dogs: [],
+      };
+    case ALLTEMP:
+      return { ...state, temperament: payload };
+    case DOG_ERROR:
+      return {
+        ...state,
+        searchError: true,
+        dogs: [],
+        allDogs: [],
+      };
+
+    case ORDER:
+      let orderedDogs = [...state.dogs];
+      let orderedAllDogs = [...state.allDogs];
+
+      switch (payload) {
+        case "A":
+          orderedDogs?.sort((a, b) => a.name.localeCompare(b.name));
+          orderedAllDogs?.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case "D":
+          orderedDogs?.sort((a, b) => b.name.localeCompare(a.name));
+          orderedAllDogs?.sort((a, b) => b.name.localeCompare(a.name));
+          break;
+        case "minWeight":
+          orderedDogs?.sort((a, b) => {
+            let pesoA;
+            let pesoB;
+
+            if (!a.weight) {
+              pesoA = a.peso?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            } else {
+              pesoA = a.weight?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            }
+            if (!b.weight) {
+              pesoB = b.peso?.split(" - ");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            } else {
+              pesoB = b.weight?.split(" - ");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            }
+
+            return parseInt(pesoA[0]) - parseInt(pesoB[0]);
+          });
+          orderedAllDogs?.sort((a, b) => {
+            let pesoA;
+            let pesoB;
+
+            if (!a.weight) {
+              pesoA = a.peso?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            } else {
+              pesoA = a.weight?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            }
+            if (!b.weight) {
+              pesoB = b.peso?.split(" - ");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            } else {
+              pesoB = b.weight?.split(" - ");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            }
+
+            return parseInt(pesoA[0]) - parseInt(pesoB[0]);
+          });
+
+          break;
+        case "maxWeight":
+          orderedDogs?.sort((a, b) => {
+            let pesoA;
+            let pesoB;
+
+            if (!a.weight) {
+              pesoA = a.peso?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            } else {
+              pesoA = a.weight?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            }
+
+            if (!b.weight) {
+              pesoB = b.peso?.split("-");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            } else {
+              pesoB = b.weight?.split("-");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            }
+            return parseInt(pesoB) - parseInt(pesoA);
+          });
+          orderedAllDogs?.sort((a, b) => {
+            let pesoA;
+            let pesoB;
+
+            if (!a.weight) {
+              pesoA = a.peso?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            } else {
+              pesoA = a.weight?.split("-");
+              pesoA = pesoA.length > 1 ? pesoA[1] : pesoA[0];
+            }
+
+            if (!b.weight) {
+              pesoB = b.peso?.split("-");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            } else {
+              pesoB = b.weight?.split("-");
+              pesoB = pesoB.length > 1 ? pesoB[1] : pesoB[0];
+            }
+            return parseInt(pesoB) - parseInt(pesoA);
+          });
+          break;
+
+        default:
+          break;
+      }
+      return {
+        ...state,
+        dogs: orderedDogs,
+        allDogs: orderedAllDogs,
+        orderAndFilter: {
+          ...state.orderAndFilter,
+          order: payload ? payload : "A",
+        },
+      };
+
+    case FILTER_TEMP:
+      if (payload === "All") {
+        return {
+          ...state,
+          dogs: state.allDogs,
+          orderAndFilter: {
+            ...state.orderAndFilter,
+            tempFilter: payload,
+          },
+          searchError: state.allDogs.length > 0 ? false : true,
+        };
+      } else {
+        let filteredDogs = state.allDogs.filter((dog) =>
+          dog?.temperament?.includes(payload)
+        );
+
+        return {
+          ...state,
+          dogs: filteredDogs,
+          orderAndFilter: {
+            ...state.orderAndFilter,
+            tempFilter: payload,
+            originFilter: "All",
+          },
+          searchError: filteredDogs.length ? false : true,
+        };
+      }
+
+    case FILTER_ORIGIN:
+      if (payload === "All") {
+        return {
+          ...state,
+          dogs: state.allDOgs,
+          orderAndFilter: {
+            ...state.orderAndFilter,
+            tempFilter: payload,
+          },
+          searchError: state.allDogs.length > 0 ? false : true,
+        };
+      } else {
+        let filteredDogs = [];
+        if (payload === "real")
+          filteredDogs = state.allDogs.filter(
+            (dog) => typeof dog?.id === "number"
+          );
+        else if (payload === "created")
+          filteredDogs = state.allDogs.filter(
+            (dog) => typeof dog?.id === "string"
+          );
+
+        return {
+          ...state,
+          dogs: filteredDogs,
+          orderAndFilter: {
+            ...state.orderAndFilter,
+            originFilter: payload,
+            tempFilter: "All",
+          },
+          searchError: filteredDogs.length ? false : true,
+        };
+      }
+    case FILTER_LIFE:
+      let filteredDogsByLife;
+      console.log(payload);
+      if (payload === "Short") {
+        filteredDogsByLife = state.allDogs.filter((dog) => {
+          const firstPart = dog.life_span.split("-")[0].slice(0, 2);
+          return parseInt(firstPart) >= 10 && parseInt(firstPart) <= 13;
+        });
+      } else if (payload === "Medium") {
+        filteredDogsByLife = state.allDogs.filter((dog) => {
+          const firstPart = dog.life_span.split("-")[0].slice(0, 2);
+          return parseInt(firstPart) >= 13 && parseInt(firstPart) < 15;
+        });
+      } else {
+        filteredDogsByLife = state.allDogs.filter((dog) => {
+          const firstPart = dog.life_span.split("-")[0].slice(0, 2);
+          return parseInt(firstPart) >= 15;
+        });
+      }
+      return {
+        ...state,
+        dogs: filteredDogsByLife,
+        orderAndFilter: {
+          ...state.orderAndFilter,
+          originFilter: "all",
+          tempFilter: "All",
+        },
+        searchError: filteredDogsByLife.length > 0 ? false : true,
+      };
+    default:
+      return { ...state };
+  }
+}
